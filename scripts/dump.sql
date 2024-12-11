33c43548b58d269224f411f08ee575ffb5a59696
Benjamin Grimstad <bae12337@gmail.com>
2024-12-11 14:58:08 +0100

    v2.1

--- scripts/dump.sql
+++ scripts/dump.sql
@@ -5,10 +5,10 @@ INSERT INTO products (price, type, productName, imgsrc) VALUES (40, 'drink', 'An
 INSERT INTO products (price, type, productName, imgsrc) VALUES (25, 'drink', 'Espresso', 'https://res.cloudinary.com/dz9rv9fgo/image/upload/v1732785855/espresso_xnat8j.jpg');
 INSERT INTO products (price, type, productName, imgsrc) VALUES (30, 'drink', 'Cappuccino', 'https://res.cloudinary.com/dz9rv9fgo/image/upload/v1732785853/cappuccino_aspkdb.jpg');
 
-INSERT INTO products (price, type, productName, imgsrc) VALUES (65, 'sandwich', 'Avocado', 'https://joepay-api.joejuice.com/productAssets/47?type=tile_row&resolution=1');
-INSERT INTO products (price, type, productName, imgsrc) VALUES (59, 'sandwich', 'Tunacado', 'https://joepay-api.joejuice.com/productAssets/51?type=tile_row&resolution=1');
-INSERT INTO products (price, type, productName, imgsrc) VALUES (69, 'sandwich', 'Turkey', 'https://joepay-api.joejuice.com/productAssets/52?type=tile_row&resolution=1');
-INSERT INTO products (price, type, productName, imgsrc) VALUES (69, 'sandwich', 'Club', 'https://joepay-api.joejuice.com/productAssets/48?type=tile_row&resolution=1');
-INSERT INTO products (price, type, productName, imgsrc) VALUES (50, 'sandwich', 'Spicy Tuna', 'https://joepay-api.joejuice.com/productAssets/48?type=tile_row&resolution=1');
+INSERT INTO products (price, type, productName, imgsrc) VALUES (65, 'sandwich', 'Avocado', 'https://res.cloudinary.com/dz9rv9fgo/image/upload/v1733925098/Avocado_hq0tim.jpg');
+INSERT INTO products (price, type, productName, imgsrc) VALUES (59, 'sandwich', 'Tunacado', 'https://res.cloudinary.com/dz9rv9fgo/image/upload/v1733925161/Tunacado_okkpxv.jpg');
+INSERT INTO products (price, type, productName, imgsrc) VALUES (69, 'sandwich', 'Turkey', 'https://res.cloudinary.com/dz9rv9fgo/image/upload/v1733925281/Turkey_bteatu.jpg');
+INSERT INTO products (price, type, productName, imgsrc) VALUES (69, 'sandwich', 'Club', 'https://res.cloudinary.com/dz9rv9fgo/image/upload/v1733925336/Club_pfvwdn.jpg');
+INSERT INTO products (price, type, productName, imgsrc) VALUES (50, 'sandwich', 'Spicy Tuna', 'https://res.cloudinary.com/dz9rv9fgo/image/upload/v1733925380/Spicy_Tuna_oiissd.jpg');
 
 INSERT INTO customers (username, email, password) VALUES ('test', 'test@test.dk', '$2b$10$5rhAqLjRayf13iYFCIYpMej8y0PxzeyWjTC7jyvpSkrH3PHM8Q3mS');
